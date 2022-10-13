ccd600ca5bb7940ab9743de7d5b9afda9a305bed
Avisek Pandit <simonp4466@gmail.com>
2022-10-13 14:16:47 -0400

    add open fishcord button

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -36,6 +36,9 @@ function HomePage() {
                     <h1>IMAGINE A PLACE...</h1>
                     <p>...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.</p>
                 </div>
+                <div className="nav-open-fishcord">
+                        {sessionUser ? <NavLink className="nav-open-fishcord-text" to="/">Open Fishcord</NavLink> : <NavLink className="nav-open-fishcord-text" to="/login">Open Fishcord</NavLink> }
+                </div>
             </div>
             <div className='splash-blue-line'></div>
             <div className="splash-invite-only">
