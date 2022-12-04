42bd96e103242992a38ae6eb1d976b3894b2c5d9
Avisek Pandit <simonp4466@gmail.com>
2022-12-04 14:27:53 -0500

    testing allow request origin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,9 +70,9 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.web_socket_server_url = "wss://fishcord.herokuapp.com/cable" 
-  config.action_cable.allowed_request_origins = ['https://fishcord.herokuapp.com', 'http://fishcord.herokuapp.com']
 
+  config.web_socket_server_url = "wss://fishcord.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://fishcord.herokuapp.com', 'https://fishcord.herokuapp.com']
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
