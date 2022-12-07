30152bffe86fd09d1568ed2afd28d75ec468bae5
Avisek Pandit <simonp4466@gmail.com>
2022-12-07 16:34:00 -0500

    testing action_cable mount_path nil

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
+  config.action_cable.mount_path = nil
+  config.action_cable.url = "wss://fishcord.herokuapp.com//cable"
+  config.action_cable.allowed_request_origins = [ "https://fishcord.herokuapp.com/", "https://fishcord.herokuapp.com/" ]  
   
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
