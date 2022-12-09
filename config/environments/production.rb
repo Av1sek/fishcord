fbe4eed39f89e0abb794dfdbdf416c2ebc833149
Avisek Pandit <simonp4466@gmail.com>
2022-12-09 16:42:51 -0500

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,6 +40,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   config.action_cable.mount_path = nil
   config.action_cable.url = "wss://fishcord.herokuapp.com/cable"
+  config.web_socket_server_url = "wss://fishcord.herokuapp.com/cable"
   config.action_cable.allowed_request_origins = [ "https://fishcord.herokuapp.com" ]  
   
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
